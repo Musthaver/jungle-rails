ceaede78a9f0d364f09c37d407343d0eb3e0d95d
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-13 18:57:33 +0000

    added email order confirmation capability

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      ReceiptMailer.receipt_email(order).deliver_now
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
@@ -17,7 +18,13 @@ class OrdersController < ApplicationController
 
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
+
+
+
+    
   end
+
+
 
   private
 
---
+++ app/mailers/receipt_mailer.rb
@@ -0,0 +1,9 @@
+class ReceiptMailer < ApplicationMailer
+    default from: 'no-reply@jungle.com'
+ 
+    def receipt_email(order)
+      @order = order
+      mail(to: @order.email, subject: Order @order.id) 
+    end
+end
+
