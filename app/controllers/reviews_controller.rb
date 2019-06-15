bb8c4fa7f7cc6ce4ebb8f8a55589f0a226190e77
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-15 20:59:59 +0000

    added delete button that works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,19 @@ class ReviewsController < ApplicationController
         @review.user = current_user
 
         if @review.save
-          redirect_to products_path, notice: 'Review created!'
+          redirect_to @product, notice: 'Review created!'
         else
-            redirect_to products_path
+            redirect_to @product
+            flash[:error] = 'An error occured!'
             puts @review.errors.full_messages
         end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        @product = Product.find(params[:product_id])
+        redirect_to @product, notice: 'Review deleted!'
     end
 
     private
