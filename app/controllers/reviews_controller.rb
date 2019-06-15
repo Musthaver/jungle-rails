732970184c9b3ff70ecdb9f0fe27bfbf9b498afe
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-15 19:31:22 +0000

    Added ability to create a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,26 @@
 class ReviewsController < ApplicationController
-
+    before_filter :deny_access, :unless => :current_user
+    
     def create
-        @review = Review.where("product_id = ?", params[:product_id]).new #find(params[:product_id])
-        puts params
+        @product = Product.find(params[:product_id])
+        @review = @product.reviews.create(review_params)
+        @review.user = current_user
+
+        if @review.save
+          redirect_to products_path, notice: 'Review created!'
+        else
+            redirect_to products_path
+            puts @review.errors.full_messages
+        end
+    end
+
+    private
+
+    def review_params
+      params.require(:review).permit(
+        :rating,
+        :description
+      )
     end
 
 end
